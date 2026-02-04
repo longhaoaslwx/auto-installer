da4a95917fec73edad09a1d9b063d83c49790dc9
龍皓 <qq1170764787@outlook.com>
2026-02-04 17:03:05 +0800

    项目初始化

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2026/2/4 17:02
+# @File    : __init__.py.py
+# @Project : auto-installer
+# @Author  : Longhao
+# @Desc    : 
+# @Version : 1.0.0
+# --------------------------------------
